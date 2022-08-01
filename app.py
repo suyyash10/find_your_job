e5e05b1e6d8f06f43936e5a47940b4c5362f313a
saurabhbhoir11 <20102076.saurabh.bhoir@gmail.com>
2022-08-01 22:07:05 +0530

    Updated Working Database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import *
+from flask_mysqldb import * 
+from sqlalchemy import *
+import mysql.connector
 
 app = Flask(__name__)
+
+#database Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'findmyjob'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
@@ -10,8 +21,19 @@ def home():
 def login():
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup" , methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        userDetails = request.form
+        username = userDetails['username']
+        user_email = userDetails['user_email']
+        user_password = userDetails['user_password']
+        cursor = mysql.connection.cursor()
+        cursor.execute("insert into BasicData values(%s, %s,%s)", (username, user_email, user_password))
+        cursor.execute("insert into loginData values(%s, %s)", (username, user_password))
+        mysql.connection.commit()
+        cursor.close()
+
     return render_template("signup.html")
 
 if __name__ == "__main__":
