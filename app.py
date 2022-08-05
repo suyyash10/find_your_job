c6d49381a71f3a271358e4ffc0ed6ed4c6b5b582
saurabhbhoir11 <20102076.saurabh.bhoir@gmail.com>
2022-08-05 17:05:28 +0530

    added user login

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import *
-from flask_mysqldb import * 
+from flask import Flask, request, render_template, url_for, session, redirect
+from flask_mysqldb import *
 from sqlalchemy import *
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key="mykey"
 
 #database Configuration
 app.config['MYSQL_HOST'] = 'localhost'
@@ -17,8 +18,23 @@ mysql = MySQL(app)
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login" , methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        loginDetails = request.form
+        username = loginDetails['username']
+        password = loginDetails['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute("select * from loginData where username=%s and password=%s", (username, password,))
+        details = cursor.fetchone()
+        if details:
+            session['loggedIn']= True
+            session['username']= details[0]
+            return redirect(url_for('home'))
+        else:
+            msg="Incorrrect Username/Password"
+            return render_template("login.html", msg=msg)               
+        cursor.close()
     return render_template("login.html")
 
 @app.route("/signup" , methods=['GET', 'POST'])
@@ -29,8 +45,11 @@ def signup():
         user_email = userDetails['user_email']
         user_password = userDetails['user_password']
         cursor = mysql.connection.cursor()
-        cursor.execute("insert into BasicData values(%s, %s,%s)", (username, user_email, user_password))
-        cursor.execute("insert into loginData values(%s, %s)", (username, user_password))
+        cursor.execute("select username from loginData where username = %s", (username,))
+        test = cursor.fetchone()
+        if not test:
+            cursor.execute("insert into BasicData values(%s, %s)", (username, user_email))
+            cursor.execute("insert into loginData values(%s, %s)", (username, user_password))
         mysql.connection.commit()
         cursor.close()
 
